0a8de7ffc0dcd8deb7fd14982d435301c8f11ba7
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-08-16 14:49:25 -0300

    aula 17 criada

---
+++ JavaScript Udemy/node/aula17/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeControllers');
+const contatoController = require('./src/controllers/contatoController');
+module.exports = route;
+
+
+
+// Rotas da Home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+// Rotas Contato
+route.get('/contato', contatoController.paginaContato);
