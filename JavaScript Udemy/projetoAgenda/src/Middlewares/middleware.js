192cca9ab5cf89f3595925b85773c84a1d10afa3
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-08-28 19:30:30 -0300

    página login criada

--- JavaScript Udemy/projetoAgenda/src/Middlewares/middleware.js
+++ JavaScript Udemy/projetoAgenda/src/Middlewares/middleware.js
@@ -1,15 +1,16 @@
 exports.meuMiddleware = (req, res, next) => {
     res.locals.umaVariavelLocal = 'Este é o valor da variável local'
     next();
-}
+};
 
 exports.checkCsrfErro = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code ){
+    if(err){
         return res.render('404');
     }
-}
+    next();
+};
 
 exports.csrfToken = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
-    next()
-}
+    next();
+};
