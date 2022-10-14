858dc0b0bf43f4a16d754c6c26f0fc362da31e28
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-10-14 17:36:16 -0300

    Sequelize configurado

--- JavaScript Udemy/API_Rest/src/controllers/HomeController.js
+++ JavaScript Udemy/API_Rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeControler {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoALuno = await Aluno.create({
+      nome: 'Gui',
+      sobrenome: 'Souza',
+      email: 'guigui123@hotmail.com',
+      idade: 29,
+      peso: 98,
+      altura: 1.80,
     });
+    res.json(novoALuno);
   }
 }
 
