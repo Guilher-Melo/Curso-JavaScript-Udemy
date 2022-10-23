1546eddf4061577c96457099b454f7acb1df97a1
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-10-23 14:13:40 -0300

    middleware de verificação

--- JavaScript Udemy/API_Rest/src/controllers/UserController.js
+++ JavaScript Udemy/API_Rest/src/controllers/UserController.js
@@ -16,6 +16,8 @@ class UserControler {
   async index(req, res) {
     try {
       const users = await User.findAll();
+      console.log('USER ID', req.userId);
+      console.log('USER EMAIL', req.userEmail);
       return res.json(users);
     } catch (e) {
       return res.json(null);
