f7209b472034555900584e7f38a908d48074b35e
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-10-23 17:29:22 -0300

    corrigindo erros de segurança

--- JavaScript Udemy/API_Rest/src/routes/userRoutes.js
+++ JavaScript Udemy/API_Rest/src/routes/userRoutes.js
@@ -4,12 +4,14 @@ import userController from '../controllers/UserController';
 import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
-
-router.post('/', userController.store);
-router.get('/', loginRequired, userController.index);
+// Não deveriam existir
+router.get('/', userController.index);
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+
+// Esses deveriam existir
+router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
 
