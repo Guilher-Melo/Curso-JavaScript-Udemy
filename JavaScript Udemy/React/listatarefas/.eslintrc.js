db8252e8f5bf54cfc6669ebc049fa54919b2ea17
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2022-11-07 17:53:27 -0300

    Ambiente react criado

---
+++ JavaScript Udemy/React/listatarefas/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+  ],
+  overrides: [
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: [
+    '@babel/eslint-parser',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
