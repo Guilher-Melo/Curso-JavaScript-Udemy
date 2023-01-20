0b0f8e2db6726d60d8809475679eac2dc6c5dd2e
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-01-20 17:10:01 -0300

    Usando state

--- JavaScript Udemy/ReactRouterDom/react-router-dom/src/components/About/index.tsx
+++ JavaScript Udemy/ReactRouterDom/react-router-dom/src/components/About/index.tsx
@@ -1,6 +1,11 @@
+import { useLocation } from 'react-router-dom';
+import './style.css';
+
 export const About = () => {
+  const { state } = useLocation();
   return <div>
     <h1>About</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sint omnis, cupiditate harum earum nisi deserunt. Quidem, in non molestias dolorum nisi quae officia nobis corrupti, assumenda reprehenderit nostrum quam.</p>
+    <p>{ state }</p>
   </div>
 }
